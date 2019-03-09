1364a8b25726ffca6831d8bc6b1076bbb626d419
zhuchcn <zhuchcn@gmail.com>
2019-03-08 16:56:34 -0800

    app only

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './srcjs/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'inst/shiny-metabase/www/cytoscape')
-    }
-}
